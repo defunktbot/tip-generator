c52f917fe554651822e8500685528a3e56195ebd
defunktbot <21ricgac@waipahuh.k12.hi.us>
2020-01-29 10:57:19 -1000

    second commit

--- calculator.js
+++ calculator.js
@@ -1,5 +1,17 @@
 /* 
-Richiel Galacgac
-Period3
-Coding 1
+ * Richiel Galacgac
+ * Period 3
+ * Coding 1
 */
+
+function getBillNumber() {
+	var billNum = document.getElementById("billNumber").value;
+}
+
+function calculatePercentage() {
+	var tipPercent = document.getElementById("tipPercent").value;
+
+	var percentage = tipPercent / 100; //divides tip percent entered by 100
+	var calculatedPercentage = billNum * percentage; // multiplies percentage with bill amount
+	document.getElementById("tip").innerHTML = calculatedPercentage; // displays results?
+}
